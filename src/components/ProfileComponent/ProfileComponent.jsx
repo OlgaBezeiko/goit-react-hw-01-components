dc5996588bc11b661dce370778e43d43e267c765
OlgaBezeiko <122458798+OlgaBezeiko@users.noreply.github.com>
2023-07-21 13:21:07 +0300

    task 1

---
+++ src/components/ProfileComponent/ProfileComponent.jsx
@@ -0,0 +1,22 @@
+import CreateUsersProfile from '..Profile';
+import user from '../../Data/user.json';
+
+const ProfileComponent = () => {
+  return (
+    <div>
+      <CreateUsersProfile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+};
+
+export default ProfileComponent;
+
+
+
+
