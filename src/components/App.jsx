ff0ee88df16b7f5cf8782e909ffa42bcb96e76bf
OlgaBezeiko <122458798+OlgaBezeiko@users.noreply.github.com>
2023-07-23 14:16:25 +0300

    дата

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import { Profile } from './Profile/Profile';
 import { Statistic } from './Statistic/Statistic';
 import { FriendList } from './FriendList/FriendList';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory'
-import user from 'components/Profile/user.json';
-import data from 'components/Statistic/data.json';
-import friends from 'components/FriendList/friends.json'
-import transactions from 'components/TransactionHistory/transactions.json'
+import user from './data/user.json';
+import data from './data/data.json';
+import friends from './data/friends.json'
+import transactions from './data/transactions.json'
 
 export const App = () => {
   return (
